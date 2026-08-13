4fe6981ef8cbb38c27562eecda36ac6f736b20c7
Alquemis <maria.barquero@alquemis.com>
2026-08-13 17:09:50 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker — v20260813-5
-const CACHE = 'ventas-20260813-5';
+// Service Worker — v20260813-6
+const CACHE = 'ventas-20260813-6';
 
 const PRECACHE = [
   'index.html', 'su.html', 'admin.html',
